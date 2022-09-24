fe51ce9074ca651ed5693810229806f1cfef45d2
umeduck <ume_duck@outlook.jp>
2022-09-24 17:33:21 +0900

    商品出品情報のモデルとテーブル作成

---
+++ db/migrate/20220924081618_create_products.rb
@@ -0,0 +1,17 @@
+class CreateProducts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :products do |t|
+      t.string :name,                       null: false
+      t.text :explanation,                  null: false
+      t.integer :category_id,               null: false
+      t.integer :price,                     null: false
+      t.integer :state_id,                  null: false
+      t.integer :load_id,                   null: false
+      t.integer :area_id,                   null: false
+      t.integer :delivery_day_id,           null: false
+      t.references :user,                   null: false, forein_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_23_062918) do
+ActiveRecord::Schema.define(version: 2022_09_24_081618) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "explanation", null: false
+    t.integer "category_id", null: false
+    t.integer "price", null: false
+    t.integer "state_id", null: false
+    t.integer "load_id", null: false
+    t.integer "area_id", null: false
+    t.integer "delivery_day_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
